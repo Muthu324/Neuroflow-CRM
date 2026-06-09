fabdd65d4d7af516eb851bd5ddedf716f16ff14e
Muthu324 <muthurajp064@gmail.com>
2026-06-09 19:10:17 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './app/authContext';
 import { DataProvider } from './app/dataContext';
+import { AppProvider } from './app/providers';
 import { AppRouter } from './app/router';
 import { FloatingAlerts } from './components/notifications/FloatingAlerts';
 
@@ -9,13 +10,15 @@ export default function App() {
   return (
     <AuthProvider>
       <DataProvider>
-        <HashRouter>
-          {/* Dynamic Flying Toasts element */}
-          <FloatingAlerts />
-          
-          {/* Navigation router maps */}
-          <AppRouter />
-        </HashRouter>
+        <AppProvider>
+          <HashRouter>
+            {/* Dynamic Flying Toasts element */}
+            <FloatingAlerts />
+            
+            {/* Navigation router maps */}
+            <AppRouter />
+          </HashRouter>
+        </AppProvider>
       </DataProvider>
     </AuthProvider>
   );
